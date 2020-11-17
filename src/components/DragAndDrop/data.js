17a22e0e8a637ed9db84e46d8d64a357e1aaa15d
LuisAlpachino <alamilla-96@hotmail.com>
2020-11-17 09:03:13 -0600

    Se añadio el drag and drop

---
+++ src/components/DragAndDrop/data.js
@@ -0,0 +1,56 @@
+const data = [{
+    id: 1,
+    icon: "⭕️",
+    status: "enfermedades",
+    title: "Human Interest Form",
+    content: "Acromegalia"
+}, {
+    id: 2,
+    icon: "⭕️",
+    status: "enfermedades",
+    title: "Purchase present",
+    content: "Artritis"
+}, {
+    id: 3,
+    icon: "⭕️",
+    status: "enfermedades",
+    title: "Invest in investments",
+    content: "Artrosis"
+}, {
+    id: 4,
+    icon: "⭕️",
+    status: "enfermedades",
+    title: "Daily reading",
+    content: "Enfermedad de Paget"
+},
+{
+    id: 5,
+    icon: "⭕️",
+    status: "enfermedades",
+    title: "Daily reading",
+    content: "Otra enfermedad"
+}
+];
+
+const statuses = [{
+    status: "enfermedades",
+    icon: "⭕️",
+    color: "#EB5A46"
+}, {
+    status: "misEnfermedades",
+    icon: "🔆️",
+    color: "#00C2E0"
+}
+    // , {
+    //     status: "in review",
+    //     icon: "📝",
+    //     color: "#C377E0"
+    // }, {
+    //     status: "done",
+    //     icon: "✅",
+    //     color: "#3981DE"
+    // }
+];
+
+
+export { data, statuses };
