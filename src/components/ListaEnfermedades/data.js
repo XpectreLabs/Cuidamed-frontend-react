6ff3fdf2be1f78df508b786a5f1fdeae2584f6e6
Carlos Eduardo <c.eduardo.jesus@outlook.com>
2020-11-21 13:17:57 -0600

    cambio de diseno de carpeta y colores del titulo

--- src/components/ListaEnfermedades/data.js
+++ src/components/ListaEnfermedades/data.js
@@ -16,52 +16,61 @@ const arrayIconHumanSys = [
         name: "óseo",
         component: <Oseo></Oseo>,
         system: "Sistema óseo",
-        color: "#43ffdf",
+        color: "#2b19a0",
     },
     {
         name: "respiratorio",
         component: <Respiratorio></Respiratorio>,
         system: "Sistema respiratorio",
+        color: "#2c6d81",
     },
     {
         name: "reproductivo",
         component: <Reproductivo></Reproductivo>,
         system: "Sistema reproductivo",
+        color: "#ff1695",
     },
     {
         name: "piel",
         component: <Piel></Piel>,
         system: "Piel",
+        color: "#81452c",
     },
     {
         name: "endocrino",
         component: <Endocrino></Endocrino>,
         system: "Sistema endocrino",
+        color: "#a07319",
     },
     {
         name: "urinario",
         component: <Urinario></Urinario>,
         system: "Sistema urinario",
+        color: "#86a019",
     },
     {
         name: "muscular",
         component: <Muscular></Muscular>,
         system: "Sistema muscular",
+        color: "#772d11",
     },
     {
         name: "nervioso",
         component: <Nervioso></Nervioso>,
         system: "Sistema nervioso",
+        color: "#32812c",
     },
     {
         name: "digestivo",
         component: <Digestivo></Digestivo>,
         system: "Sistema digestivo",
+        color: "#19a054",
     },
     {
         name: "circulatorio",
         component: <Circulatorio></Circulatorio>,
         system: "Sistema circulatorio",
+        color: "#a01919",
     }
 
 ]
