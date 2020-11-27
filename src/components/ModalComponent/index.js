9c72a7f8de77d571dffc6add9805f4450c808673
LuisAlpachino <alamilla-96@hotmail.com>
2020-11-27 16:34:53 -0600

    Se añadio el buscador y el agregar enfermedades no encontradas

--- src/components/ModalComponent/index.js
+++ src/components/ModalComponent/index.js
@@ -1,32 +1,34 @@
 import React, { useState } from 'react';
-import { Modal, Button, Header, Image } from 'semantic-ui-react';
+import { Modal, } from 'semantic-ui-react';
 import { CustomInput } from '../inputsCustom/CustomInput';
 import { Carpeta } from '../../images/icons/icons'
 
-const ModalComponent = ({ 
-  open = false, 
-  onOpen, 
-  onClose, 
+const ModalComponent = ({
+  open = false,
+  onOpen,
+  onClose,
   title = 'Modal',
   textModal = '',
   buttonText = '',
   placeholder = '',
-  icon = false
+  icon = false,
+  setInputData = (e) => { },
+  onClick,
 }) => {
   const [code, setCode] = useState('');
   return (
     <Modal open={open} onClose={onClose} onOpen={onOpen}>
-      <Modal.Content className={`${icon ? 'modal-enfermedades' : '' }`}>
+      <Modal.Content className={`${icon ? 'modal-enfermedades' : ''}`}>
         <h1 style={{ textAlign: 'center' }} className="modal__title">
           {title}
         </h1>
         <div>
           <form className="modal-form">
-            
+
             <p className="modal-form__paragraph">
               {textModal}
             </p>
-            <div className={`icon-carpeta ${icon ? '' : 'hidden-icon' } `}>
+            <div className={`icon-carpeta ${icon ? '' : 'hidden-icon'} `}>
               <Carpeta />
             </div>
             <div className="modal-form__input-container">
@@ -37,11 +39,12 @@ const ModalComponent = ({
                 smallStyle={true}
                 setValue={(e) => {
                   setCode(e);
+                  setInputData(e);
                 }}
               />
             </div>
             <div className="modal-form__button-submit-container">
-              <button type="submit" className="modal-form__button-submit">
+              <button type="button" onClick={onClick} className="modal-form__button-submit">
                 {buttonText}
               </button>
             </div>
