8cc92071a1e4686b3e7fe7b6c6e9af2b22ea95b9
Sergio Arturo Sosa González <sergioarsosa95@gmail.com>
2020-12-30 17:25:45 -0600

    Se actualizo la pasarela de pagos

--- src/components/Plate/Plate.js
+++ src/components/Plate/Plate.js
@@ -1,16 +1,17 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { loadStripe } from "@stripe/stripe-js";
-import { Grid, Input, Button } from 'semantic-ui-react';
-import Logo from '../../images/CuidaMEDLogo.png';
-import Placa from '../../images/placa.jpg';
-import { CustomInput } from '../inputsCustom/CustomInput';
-import { SelectCustom } from '../inputsCustom/Select/Select';
-import bloodType from './data';
+import { Grid, Input, Button } from "semantic-ui-react";
+import Logo from "../../images/CuidaMEDLogo.png";
+import Placa from "../../images/placa.jpg";
+import { CustomInput } from "../inputsCustom/CustomInput";
+import { SelectCustom } from "../inputsCustom/Select/Select";
+import bloodType from "./data";
+import { CONECTION } from "../../conection";
 
-const stripePromise = loadStripe('KEY STRIPE');
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_KEY);
 
 export default function Plate() {
-    const [message, setMessage] = useState("");
+  const [message, setMessage] = useState("");
 
   useEffect(() => {
     // Check to see if this is a redirect back from Checkout
@@ -21,9 +22,7 @@ export default function Plate() {
     }
 
     if (query.get("canceled")) {
-      setMessage(
-        "Order canceleda,Intente de nuevo."
-      );
+      setMessage("Order canceleda,Intente de nuevo.");
     }
   }, []);
 
@@ -32,7 +31,14 @@ export default function Plate() {
     const stripe = await stripePromise;
 
     // Call your backend to create the Checkout Session
-    const response = await fetch('http://localhost:1337/api/create-checkout-session', { method: 'POST' });
+    const response = await fetch(`${CONECTION}api/create-checkout-session`, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+        "x-auth-token": localStorage.getItem("refreshToken"),
+      },
+    });
 
     const session = await response.json();
     console.log(session);
@@ -49,105 +55,118 @@ export default function Plate() {
     }
   };
 
-  
-    const [formValues, setFormValues] = useState({
-        name: "",
-        emergencyContact: "",
-        typeBlood: "",
-        disease: "",
-      });
-      const { name, emergencyContact, typeBlood, disease } = formValues;
+  const [formValues, setFormValues] = useState({
+    name: "",
+    emergencyContact: "",
+    typeBlood: "",
+    disease: "",
+  });
+  const { name, emergencyContact, typeBlood, disease } = formValues;
 
-    return (
-        
-        <Grid className="plate">
-            <Grid.Column width={9} verticalAlign="middle">
-                <Grid.Row className="bg-plate">
-                    <Grid.Column verticalAlign="middle">
-                        <Grid.Row >
-                            <p>{name}</p>
-                        </Grid.Row>
-                        <Grid.Row >
-                            <p>{emergencyContact}</p>
-                        </Grid.Row>
-                        <Grid.Row>
-                            <p>{typeBlood}</p>
-                        </Grid.Row>
-                        <Grid.Row>
-                            <p>{disease}</p>
-                        </Grid.Row>
-                    </Grid.Column>
-                </Grid.Row>
-            </Grid.Column>
-            <Grid.Column width={7} verticalAlign="middle">
-                <Grid.Row className="logo">
-                        <img src={Logo} />
-                </Grid.Row>
-                <Grid.Row className="description">
-                    <p>
-                        Información que tendrá la placa.
-                    </p>
-                </Grid.Row>
-                <Grid.Row className="information">
-                    <label>¿Quién usuará la pulsera?</label>
-                    <br/>
-                    <Input type="text" name='name' 
-                    placeholder='Raúl Núñez / Raúl Núñez 1991'
-                    // setValue={(e) => setFormValues({ ...formValues, email: e })}
-                    //     value={email}
-                    onChange={(e) => {
-                        setFormValues({...formValues,name: e.currentTarget.value});
-
-                    }}
-                    value={name}
-                     />
-                    <span className={name.length > 25 ? 'warning' : ''}>{name.length}/25</span>
-                </Grid.Row>
-                <Grid.Row className="information">
-                    <label>¿Quién será tu contacto de emergencia?</label>
-                    <br/>
-                    <Input type="text" name="emergency-contact" placeholder='Eduardo +529932000000'
-                        onChange={(e) => {
-                        setFormValues({...formValues,emergencyContact: e.currentTarget.value});
-
-                    }}
-                    value={emergencyContact}
-                    />
-                    <span className={emergencyContact.length > 25 ? 'warning' : ''}>{emergencyContact.length}/25</span>
-                </Grid.Row>
-                <Grid.Row className="information">
-                    <label>Tipo de sangre</label>
-                    <br/>
-                    <SelectCustom
-                        placeholder=""
-                        dataOptions={bloodType}
-                        name="type-blood"
-                        setValue={(e) => {
-                            setFormValues({ ...formValues, typeBlood: e });
-                        }}
-                        value={typeBlood}
-                    />
-                </Grid.Row>
-                <Grid.Row className="information">
-                    <label>Padecimiento principal</label>
-                    <br/>
-                    <Input type="text" placeholder='Hipertensión / Diabético / Alzheimer' name="disease" 
-                        onChange={(e) => {
-                        setFormValues({...formValues,disease: e.currentTarget.value});
-
-                    }}
-                    value={disease}
-                    />
-                    <span className={disease.length > 25 ? 'warning' : ''}>{disease.length}/25</span>
-                </Grid.Row>
-                <Grid.Row>
-                    <Button className="btn-main" id="checkout-button" role="link" onClick={handleClick}>Proceder al pago</Button>
-                    
-                </Grid.Row>
-            </Grid.Column>
-        </Grid>
-        
-    )
-    
+  return (
+    <Grid className="plate">
+      <Grid.Column width={9} verticalAlign="middle">
+        <Grid.Row className="bg-plate">
+          <Grid.Column verticalAlign="middle">
+            <Grid.Row>
+              <p>{name}</p>
+            </Grid.Row>
+            <Grid.Row>
+              <p>{emergencyContact}</p>
+            </Grid.Row>
+            <Grid.Row>
+              <p>{typeBlood}</p>
+            </Grid.Row>
+            <Grid.Row>
+              <p>{disease}</p>
+            </Grid.Row>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid.Column>
+      <Grid.Column width={7} verticalAlign="middle">
+        <Grid.Row className="logo">
+          <img src={Logo} />
+        </Grid.Row>
+        <Grid.Row className="description">
+          <p>Información que tendrá la placa.</p>
+        </Grid.Row>
+        <Grid.Row className="information">
+          <label>¿Quién usuará la pulsera?</label>
+          <br />
+          <Input
+            type="text"
+            name="name"
+            placeholder="Raúl Núñez / Raúl Núñez 1991"
+            // setValue={(e) => setFormValues({ ...formValues, email: e })}
+            //     value={email}
+            onChange={(e) => {
+              setFormValues({ ...formValues, name: e.currentTarget.value });
+            }}
+            value={name}
+          />
+          <span className={name.length > 25 ? "warning" : ""}>
+            {name.length}/25
+          </span>
+        </Grid.Row>
+        <Grid.Row className="information">
+          <label>¿Quién será tu contacto de emergencia?</label>
+          <br />
+          <Input
+            type="text"
+            name="emergency-contact"
+            placeholder="Eduardo +529932000000"
+            onChange={(e) => {
+              setFormValues({
+                ...formValues,
+                emergencyContact: e.currentTarget.value,
+              });
+            }}
+            value={emergencyContact}
+          />
+          <span className={emergencyContact.length > 25 ? "warning" : ""}>
+            {emergencyContact.length}/25
+          </span>
+        </Grid.Row>
+        <Grid.Row className="information">
+          <label>Tipo de sangre</label>
+          <br />
+          <SelectCustom
+            placeholder=""
+            dataOptions={bloodType}
+            name="type-blood"
+            setValue={(e) => {
+              setFormValues({ ...formValues, typeBlood: e });
+            }}
+            value={typeBlood}
+          />
+        </Grid.Row>
+        <Grid.Row className="information">
+          <label>Padecimiento principal</label>
+          <br />
+          <Input
+            type="text"
+            placeholder="Hipertensión / Diabético / Alzheimer"
+            name="disease"
+            onChange={(e) => {
+              setFormValues({ ...formValues, disease: e.currentTarget.value });
+            }}
+            value={disease}
+          />
+          <span className={disease.length > 25 ? "warning" : ""}>
+            {disease.length}/25
+          </span>
+        </Grid.Row>
+        <Grid.Row>
+          <Button
+            className="btn-main"
+            id="checkout-button"
+            role="link"
+            onClick={handleClick}
+          >
+            Proceder al pago
+          </Button>
+        </Grid.Row>
+      </Grid.Column>
+    </Grid>
+  );
 }
-
